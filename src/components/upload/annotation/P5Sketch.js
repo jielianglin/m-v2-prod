b4240be68353632ff10119b64c5b222eccd2243a
Jielianglin <jielianglin821@gmail.com>
2021-05-27 15:28:48 +0200

    integrated image preload function and p5 graphics renderer for pencil tool

--- src/components/upload/annotation/P5Sketch.js
+++ src/components/upload/annotation/P5Sketch.js
@@ -1,29 +1,43 @@
 import Sketch from 'react-p5';
+import Sample from './sample/Sample.jpg';
 
 let penSize = 2;
 let penState = 0;
 
 let colorPicker;
 let height; 
-let color; 
+let color;
+
+let img; 
+let pg; 
 
 export default function Marker() {
+    
+    const preload = p5 => {
+    img = p5.loadImage(`${Sample}`);
+    }
+
     const setup = (p5, canvasParentRef) => {
         p5.createCanvas(600,600).parent(canvasParentRef);
-        p5.background(225);
+        pg = p5.createGraphics(600, 600); 
         colorPicker = p5.createColorPicker('#ed225d');
         colorPicker.position(0, height + 5);
     }
 
+
+
     const draw = p5 => {
-       if(p5.mouseIsPressed) {
-            if (penState === 0){
-                p5.line(p5.mouseX, p5.mouseY,p5.pmouseX, p5.pmouseY);
-            }
-       }
-       p5.strokeWeight(penSize);
-       p5.stroke(colorPicker.color()); 
+        p5.image(img, 0, 0); 
+        if(p5.mouseIsPressed) {
+                if (penState === 0){
+                    pg.line(p5.mouseX, p5.mouseY,p5.pmouseX, p5.pmouseY);
+                }
+        }
+       pg.strokeWeight(penSize);
+       pg.stroke(colorPicker.color());
+        p5.image(pg, 0, 0);
     }
 
-return <Sketch setup={setup} draw={draw}/> 
+
+return <Sketch preload={preload} setup={setup} draw={draw} /> 
 }
