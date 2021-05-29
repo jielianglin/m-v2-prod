8d748014c11103a56324c92dc682779f4d50a745
Jielianglin <jielianglin821@gmail.com>
2021-05-29 02:19:00 +0200

    p5 vector sketch tool

--- src/components/upload/annotation/P5Vector.js
+++ src/components/upload/annotation/P5Vector.js
@@ -1,33 +1,34 @@
 
 import Sketch from 'react-p5';
 
+let x, y, px, py; 
 
 export default function Vector(){
+
+
     const setup = (p5, canvasParentRef) => {
         p5.createCanvas(600,600).parent(canvasParentRef);
         p5.background(225);  
+        
     }
     
-    const draw = p5 => {
-        if(p5.mouseIsPressed){
-            p5.beginShape();
-            console.log(p5.mouseX, p5.mouseY);
-            p5.line(p5.pmouseX, p5.pmouseY,p5.mouseX,p5.mouseY );
-            p5.strokeWeight(5);
-            p5.endShape(); 
-        }
-       
+    
+    const mouseDragged = p5 => {
+        x = p5.mouseX;
+        y = p5.mouseY;
+        px = p5.pmouseX;
+        py = p5.pmouseY;
+        p5.strokeWeight(10);
+        p5.line(x,y,px,py);
     }
     
     const mouseReleased = p5 => {
         p5.strokeWeight(0);
-        p5.ellipse(p5.mouseX, p5.mouseY, 15);
-        
+        p5.ellipse(x, y, 15);  
     }
-    return <Sketch setup={setup}  draw={draw} mouseReleased={mouseReleased}/> 
+
+    return <Sketch 
+    setup={setup}  
+    mouseDragged={mouseDragged} 
+    mouseReleased={mouseReleased} />
 }
-    
-
-    
-
-    
