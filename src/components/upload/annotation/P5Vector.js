8808cdfb7a50810573d6fbb6ea805379c61a3863
Jielianglin <jielianglin821@gmail.com>
2021-05-29 20:07:57 +0200

    implemented self-closing lasso for P5Vector component

--- src/components/upload/annotation/P5Vector.js
+++ src/components/upload/annotation/P5Vector.js
@@ -1,34 +1,35 @@
-
 import Sketch from 'react-p5';
 
-let x, y, px, py; 
+let init; 
+var x, y, px, py;
+var coords = []; 
 
 export default function Vector(){
 
-
     const setup = (p5, canvasParentRef) => {
         p5.createCanvas(600,600).parent(canvasParentRef);
-        p5.background(225);  
-        
+              p5.background(225);       
     }
-    
-    
-    const mouseDragged = p5 => {
+
+    const draw = p5 => {
         x = p5.mouseX;
         y = p5.mouseY;
         px = p5.pmouseX;
         py = p5.pmouseY;
-        p5.strokeWeight(10);
-        p5.line(x,y,px,py);
-    }
-    
-    const mouseReleased = p5 => {
-        p5.strokeWeight(0);
-        p5.ellipse(x, y, 15);  
+
+        if (p5.mouseIsPressed){
+            p5.strokeWeight(10); 
+            p5.stroke('red'); 
+            p5.line(x, y, px, py); 
+            coords.push([x, y]); 
+            init = coords[0]; 
+            console.log(init);
+        }
     }
 
-    return <Sketch 
-    setup={setup}  
-    mouseDragged={mouseDragged} 
-    mouseReleased={mouseReleased} />
-}
+    const mouseReleased = p5 => { 
+        p5.line(x, y, ...init); 
+    }
+
+     return <Sketch setup={setup} draw={draw} mouseReleased={mouseReleased}/>  
+    }
