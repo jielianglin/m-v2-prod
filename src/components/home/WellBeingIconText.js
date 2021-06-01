23b21e33a065a7ddd3c4eaaf839cd8a32ab73aba
Jielianglin <jielianglin821@gmail.com>
2021-06-01 20:33:10 +0200

    centered Well-Being title

--- src/components/home/WellBeingIconText.js
+++ src/components/home/WellBeingIconText.js
@@ -4,7 +4,7 @@ import P5Mold from '../upload/annotation/P5Mold';
 export default function WellBeingIconText() {
     return(
         <div>
-        <h1>WellBeing Icon Text</h1>
+        <h1 style={{textAlign:"center"}}>Well-Being</h1>
         <P5Mold/>
         </div>
     );
