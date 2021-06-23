d4fab977789be638ddad14e0cd931fcff4b8c522
Jielianglin <jielianglin821@gmail.com>
2021-06-23 14:43:09 +0200

    created onMouseOver/onMouseOut event listeners for theme icons

--- src/components/landing/SolidarityIconButton.js
+++ src/components/landing/SolidarityIconButton.js
@@ -1,13 +1,73 @@
-import React from 'react'; 
+import React from 'react';
 import Button from "@material-ui/core/Button";
 import solidarityIcon from "./icons/solidarityIcon/solidarityIcon.png"
 
-export default function homeIconButton(props){
-    return(
-        <Button>
-    <img src={solidarityIcon} alt="" width="130px" onClick={()=>{
-        props.showSolidarityIconText();
-        }} />
-    </Button>
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > *': {
+            margin: theme.spacing(1),
+            width: theme.spacing(16),
+            height: theme.spacing(16),
+        },
+        paper: {
+            padding: theme.spacing(2),
+            textAlign: 'center',
+        },
+    },
+}));
+
+export default function homeIconButton(props) {
+    const [solidarityText, showSolidarityText] = React.useState(false);
+    const classes = useStyles();
+
+    const showText = () => {
+        showSolidarityText(true);
+        console.log('mouseover');
+    }
+
+    const showIcon = () => {
+        showSolidarityText(false);
+        console.log('mouseout');
+    }
+
+    if (solidarityText) {
+        return (
+
+            <Button
+                onClick={() => {
+                    props.showSolidarityIconText();
+                }}
+            >
+                <div className={classes.root}
+                    onMouseOut={showIcon}
+                >
+                    <Paper elevation={0} className={classes.paper}
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            backgroundColor: "transparent"
+                        }}>
+                        <Typography variant="h6"> Solidarity </Typography>
+                    </Paper>
+                </div>
+            </Button>
         );
+    } else {
+        return (
+            <img src={solidarityIcon}
+                alt=""
+                width="130px"
+                onMouseOver={showText}
+                onClick={() => {
+                    props.showSolidarityIconText();
+                }}
+            />)
+    }
 }
