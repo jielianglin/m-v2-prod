2bc9e251519f03b2f2224de1487871580acc6204
Jielianglin <jielianglin821@gmail.com>
2021-07-26 18:49:38 +0200

    p5 lasso line easing with lerp()

--- frontend/src/components/upload/annotation/P5Lasso.js
+++ frontend/src/components/upload/annotation/P5Lasso.js
@@ -3,23 +3,23 @@ import Sketch from 'react-p5';
 import ColorSelector from './ColorSelector';
 import FileInput from '../FileInput';
 
-var penSize = 5;
-var penState = 0;
-
+var smoothValue = 0.05;
 var init;
-var x, y, px, py;
-var paths = [];
+var x = 0;
+var y = 0;
+var px;
+var py;
+var path = [];
 var currentPath = [];
-
 var img;
 var pg;
 
+var weight;
 
 
 export default function P5Lasso() {
   const [color, setColor] = React.useState(['#ff0000']);
   const [image, setImage] = React.useState(null);
-
 
   function setup(p5, canvasParentRef) {
     p5.createCanvas(600, 600).parent(canvasParentRef);
@@ -30,34 +30,36 @@ export default function P5Lasso() {
   }
 
   function draw(p5) {
-    x = p5.mouseX;
-    y = p5.mouseY;
-    px = p5.pmouseX;
-    py = p5.pmouseY;
 
+    x = p5.lerp(x, p5.mouseX, smoothValue);
+    y = p5.lerp(y, p5.mouseY, smoothValue);
+    px = p5.mouseX;
+    py = p5.mouseY;
 
     p5.image(img, 0, 0);
-    pg.strokeWeight(penSize);
+    pg.strokeWeight(5);
+
 
     if (p5.mouseIsPressed) {
-      if (penState === 0) {
-        console.log(color);
-        pg.stroke(color);
-
-        pg.line(x, y, px, py);
-        currentPath.push([x, y]);
-        init = currentPath[0];
-        console.log(init);
-      }
+      console.log(color);
+      pg.fill(color);
+      pg.stroke(color);
+      pg.ellipse(x, y, 30, 30);
+      currentPath.push([x, y]);
+      init = currentPath[0];
+      console.log(init);
     }
+
     p5.image(pg, 0, 0);
   }
 
+
   function mouseReleased(p5) {
     if (image) {
+      pg.strokeWeight(30);
       pg.line(x, y, ...init);
       currentPath = [];
-      paths.push(currentPath);
+      path.push(currentPath);
     }
   }
 
@@ -67,7 +69,9 @@ export default function P5Lasso() {
       <div>
         {image && (
           <div>
-            <Sketch setup={setup} draw={draw} mouseReleased={mouseReleased} />
+            <Sketch setup={setup} draw={draw}
+              mouseReleased={mouseReleased}
+            />
           </div>
         )}
         <ColorSelector selectColor={color => setColor(color)} />
