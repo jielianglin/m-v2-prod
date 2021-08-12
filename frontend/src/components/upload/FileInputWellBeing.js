1119b45ebab4af4ee355776f8390109d07299e96
Jielianglin <jielianglin821@gmail.com>
2021-08-12 23:32:05 +0200

    Fixed WellBeingTool

---
+++ frontend/src/components/upload/FileInputWellBeing.js
@@ -0,0 +1,80 @@
+import React, { useState, useRef, useEffect } from 'react';
+import Typography from "@material-ui/core/Typography";
+
+export default function FileInput(props) {
+
+  const [text, showText] = useState(true);
+  const [src, setSrc] = useState(null);
+  const fileInput = useRef(null);
+
+  const handleImageSelection = (event) => {
+    //
+    props.selectedImage(event.target.files[0]);
+    //
+    let file = event.target.files[0];
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      setSrc(e.target.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  useEffect(() => {
+    if (src) {
+      showText(false);
+      props.showInputForms();
+
+      console.log("showInputForms");
+    }
+  }, [src]);
+
+  const openFileInput = () => {
+    fileInput.current.click();
+  };
+
+  return (
+    <div
+      className="image-input"
+      onClick={openFileInput}
+      style={{
+        width: "100%",
+        height: "400px",
+
+        color: "black",
+      }}
+
+    >
+      {text &&
+        <Typography variant="h2" style={{ color: "#000000" }}>
+          <div style={{ textAlign: "center", backgroundColor: "#E6DAC8", }}>
+            <br />
+            click
+            <br />
+            +
+            <br />
+            upload
+          </div>
+        </Typography>
+      }
+      <div style={{ display: "flex", justifyContent: "center", padding: "40px" }}>
+        <img
+
+          style={{ maxWidth: "700px", height: "100%" }}
+          className="loaded-image"
+          src={src}
+          alt=""
+        />
+      </div>
+
+      <label>
+        <input
+          ref={fileInput}
+          style={{ display: "none" }}
+          type="file"
+          accept="image/*"
+          onChange={handleImageSelection}
+        />
+      </label>
+    </div>
+  );
+}
--- frontend/src/components/upload/annotation/WellBeingTool.js
+++ frontend/src/components/upload/annotation/WellBeingTool.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from "axios";
 
-import FileInput from '../FileInput';
+import FileInputWellBeing from '../FileInputWellBeing';
 import Tags from './Tags';
 import Caption from './Caption';
 
@@ -27,9 +27,10 @@ const theme = createMuiTheme({
 });
 
 export default function WellBeingTool(props) {
-    const [image, setImage] = React.useState(null);
+
 
     const [post, setPost] = React.useState(false);
+    const [image, setImage] = React.useState(null);
     const [tags, setTags] = React.useState([]);
     const [src, setSrc] = React.useState(null);
     const [caption, setCaption] = React.useState(null);
@@ -37,6 +38,7 @@ export default function WellBeingTool(props) {
     const [returnTags, setReturnTags] = React.useState([]);
     const [returnAITags, setReturnAITags] = React.useState([]);
     const [progress, setProgress] = React.useState(false);
+    const [forms, setForms] = React.useState(false);
 
     const enteredCaption = (caption) => {
         setCaption(caption);
@@ -51,6 +53,10 @@ export default function WellBeingTool(props) {
 
     const resetCanvas = () => {
         setImage(!image);
+    }
+
+    const showInputForms = () => {
+        setForms(true);
     }
 
     async function postData() {
@@ -74,127 +80,119 @@ export default function WellBeingTool(props) {
         setProgress(false);
     }
 
-    if (image) {
-        return (
-            <div>
-                {post ?
-                    <div className="returnForm">
-                        < img
-                            style={{
-                                display: "block",
-                                margin: "0 auto",
-                                height: "50%",
-                                borderRadius: "5px",
-                            }
-                            }
-                            className="returned-image"
-                            src={src}
-                            alt=""
-                        />
+    return (
+        <div>
+            {post ?
+                <div className="returnForm">
+                    < img
+                        style={{
+                            display: "block",
+                            margin: "0 auto",
+                            height: "50%",
+                            borderRadius: "5px",
+                        }
+                        }
+                        className="returned-image"
+                        src={src}
+                        alt=""
+                    />
+                    <br />
+                    <ThemeProvider theme={theme}>
+                        <Typography variant="h6" color="primary">
+                            <div className="returned-caption">
+                                <i>"</i>
+                                <i>
+                                    {" "}
+                                    <span>{returnCaption}</span>{" "}
+                                </i>
+                                <i>"</i>
+                            </div>
+                        </Typography>
                         <br />
-                        <ThemeProvider theme={theme}>
-                            <Typography variant="h6" color="primary">
-                                <div className="returned-caption">
-                                    <i>"</i>
-                                    <i>
-                                        {" "}
-                                        <span>{returnCaption}</span>{" "}
-                                    </i>
-                                    <i>"</i>
-                                </div>
-                            </Typography>
-                            <br />
-                            <span className="tags-return">
-                                <Typography color="primary"> Your Tags: </Typography>
-                                {returnTags.map((item) => (
-                                    <Chip
-                                        className="chip1" style={{ color: "#000000", backgroundColor: "#B272CE" }}
-                                        className="returned-tags-chip"
-                                        avatar={
-                                            <Avatar style={{ color: "#E6DAC8" }}>
-                                                <div style={{ color: "#FFFFFF" }}>
-                                                    #
-                                                </div>
-                                            </Avatar>
-                                        }
-                                        key={item.id}
-                                        label={item.tag}
-                                        variant="outlined"
-                                    />
-                                ))}
-                            </span>
-                            <br />
-                            <br />
-                            <span className="ai_tags_return">
-                                <Typography color="secondary"> Tags from ImageNet AI: </Typography>
-                                {returnAITags.length === 0 && (
-                                    <Typography variant="h12" color="secondary">
-                                        <i>[couldn't identify any tags] </i>
-                                    </Typography>
-                                )}
-
-                                {returnAITags.map((item) => (
-                                    <Chip
-                                        className="chip2" style={{ color: "#000000", backgroundColor: "#FFFFFF" }}
-
-                                        className="returned-ai-tags-chip"
-                                        avatar={
-                                            <Avatar style={{ background: "#B5BCB4" }}>
-                                                <div style={{ color: "#FFFFFF" }}>
-                                                    #
-                                                </div>
-                                            </Avatar>
-                                        }
-                                        key={item.id}
-                                        label={item.tag}
-                                    />
-                                ))}
-                            </span>
-                            <br />
-                            <Button onClick={resetCanvas}>Back to WellBeing upload</Button>
-                            <br />
-                        </ThemeProvider>
-                    </div >
-
-                    :
-
-                    <div style={{ padding: "50px", backgroundColor: "#E6DAC8", borderRadius: "3px", boxShadow: "3px 3px 3px #b4beb7" }}>
-                        <Caption enteredCaption={enteredCaption} />
+                        <span className="tags-return">
+                            <Typography color="primary"> Your Tags: </Typography>
+                            {returnTags.map((item) => (
+                                <Chip
+                                    className="chip1" style={{ color: "#000000", backgroundColor: "#B272CE" }}
+                                    className="return-tags"
+                                    avatar={
+                                        <Avatar style={{ color: "#E6DAC8" }}>
+                                            <div style={{ color: "#FFFFFF" }}>
+                                                #
+                                            </div>
+                                        </Avatar>
+                                    }
+                                    key={item.id}
+                                    label={item.tag}
+                                    variant="outlined"
+                                />
+                            ))}
+                        </span>
                         <br />
-                        <div >
-                            <img src={image} alt="" style={{ width: "100%", maxWidth: "800px" }} />
-                        </div>
                         <br />
-                        <Tags selectedTags={selectedTags} />
+                        <span className="ai_tags_return">
+                            <Typography color="secondary"> Tags from ImageNet AI: </Typography>
+                            {returnAITags.length === 0 && (
+                                <Typography variant="h12" color="secondary">
+                                    <i>[couldn't identify any tags] </i>
+                                </Typography>
+                            )}
+
+                            {returnAITags.map((item) => (
+                                <Chip
+                                    className="chip2" style={{ color: "#000000", backgroundColor: "#FFFFFF" }}
+
+                                    className="return-ai-tags"
+                                    avatar={
+                                        <Avatar style={{ background: "#B5BCB4" }}>
+                                            <div style={{ color: "#FFFFFF" }}>
+                                                #
+                                            </div>
+                                        </Avatar>
+                                    }
+                                    key={item.id}
+                                    label={item.tag}
+                                />
+                            ))}
+                        </span>
                         <br />
-                        <div className="save-data">
-                            <Button
-                                disabled={!image || !caption || tags.length === 0}
-                                className="save-button"
-                                palette="primary"
-                                onClick={postData}
-                            >
-                                {" "}
-                                Save{" "}
-                            </Button>
-                            <ThemeProvider theme={theme}>
-                                {progress && <CircularProgress />}
-                            </ThemeProvider>
-                        </div>
-                    </div >
+                        <Button onClick={resetCanvas}>Back to WellBeing upload</Button>
+                        <br />
+                    </ThemeProvider>
+                </div >
 
-                }
-            </div>)
+                :
 
-    } else {
-        return (
+                <div style={{ padding: "50px", backgroundColor: "#E6DAC8", borderRadius: "3px", boxShadow: "3px 3px 3px #b4beb7" }}>
+                    {forms && <Caption enteredCaption={enteredCaption} />}
+                    <br />
+                    <div >
+                        <FileInputWellBeing selectedImage={selectedImage} showInputForms={showInputForms} />
+                    </div>
+                    <br />
+                    {forms &&
+                        <div>
+                            <br />
+                            <Tags selectedTags={selectedTags} />
+                            <br />
+                            <div className="save-data">
+                                <Button
+                                    disabled={!image || !caption || tags.length === 0}
+                                    className="save-button"
+                                    palette="primary"
+                                    onClick={postData}
+                                >
+                                    {" "}
+                                    Save{" "}
+                                </Button>
+                                <ThemeProvider theme={theme}>
+                                    {progress && <CircularProgress />}
+                                </ThemeProvider>
+                            </div>
+                        </div>}
+                </div >
+            }
 
-            <FileInput
-                //is this needed okay? 
-                selectImage={setImage}
-                selectedImage={selectedImage}
-            />
-        );
-    }
+        </div>
+    )
 }
-
